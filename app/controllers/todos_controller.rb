ebc97b4d2c8c1a7f559ec7044be7e8f7ca5dcf55
Alyssa Biot <alyssa.biot@gmail.com>
2022-02-25 17:39:07 +0100

    add tabs and handle status change

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,25 +1,20 @@
 class TodosController < ApplicationController
-  before_action :set_todo, only: %i[ show edit update destroy ]
+  before_action :set_todo, only: %i[ show edit update destroy change_status]
 
-  # GET /todos or /todos.json
   def index
-    @todos = Todo.all
+    @todos = Todo.where(status: params[:status].presence || 'incomplete')
   end
 
-  # GET /todos/1 or /todos/1.json
   def show
   end
 
-  # GET /todos/new
   def new
     @todo = Todo.new
   end
 
-  # GET /todos/1/edit
   def edit
   end
 
-  # POST /todos or /todos.json
   def create
     @todo = Todo.new(todo_params)
 
@@ -34,7 +29,6 @@ class TodosController < ApplicationController
     end
   end
 
-  # PATCH/PUT /todos/1 or /todos/1.json
   def update
   respond_to do |format|
     if @todo.update(todo_params)
@@ -49,7 +43,6 @@ class TodosController < ApplicationController
   end
 end
 
-  # DELETE /todos/1 or /todos/1.json
   def destroy
     @todo.destroy
 
@@ -60,13 +53,19 @@ end
     end
   end
 
+  def change_status
+    @todo.update(status: todo_params[:status])
+    respond_to do |format|
+      format.turbo_stream { render turbo_stream: turbo_stream.remove("#{helpers.dom_id(@todo)}_container") }
+      format.html { redirect_to todos_path, notice: "Updated todo status." }
+    end
+  end
+
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_todo
       @todo = Todo.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def todo_params
       params.require(:todo).permit(:name, :status)
     end
